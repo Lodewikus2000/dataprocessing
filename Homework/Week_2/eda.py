da32e5bd9f43c6495163c426fae6ed97ace58f96
Lodewikus2000 <32392003+Lodewikus2000@users.noreply.github.com>
2019-04-12 14:56:06 +0200

    verbeterd

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -6,12 +6,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+from scipy import stats
 
 INPUT_CSV = "input.csv"
 OUTPUT_CSV = "cleaned.csv"
 OUTPUT_JSON = "data.csv"
-
-keys = [
+NR_OF_BINS = 40
+KEYS = [
     "Country", "Region", "Pop. Density (per sq. mi.)",
     "Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"
     ]
@@ -25,24 +26,40 @@ with open(INPUT_CSV, "r", newline="\n") as infile:
     for row in reader:
         for key, value in row.items():
             value = value.replace(",",".").replace("dollars","").strip(" ")
-            if len(value) == 0:
-                value = np.nan
-            if value == "unknown":
+            if (len(value) == 0) or (value == "unknown"):
                 value = np.nan
             row[key] = value
         data.append(row)
 
+
 # Output cleaner csv.
 with open(OUTPUT_CSV, "w", newline="") as outfile:
-    writer = csv.DictWriter(outfile, fieldnames=keys, extrasaction="ignore")
+    writer = csv.DictWriter(outfile, fieldnames=KEYS, extrasaction="ignore")
     writer.writeheader()
     for row in data:
         writer.writerow(row)
 
+
 # Make list of countries to use as main index for the dataframe.
 index = [row["Country"] for row in data]
+
+
+
+
+
+# Turn the data into a dataframe.
 dataframe = pd.DataFrame(data, index=index)
 dataframe = dataframe.drop(columns="Country")
+
+
+length_before = len(dataframe.index)
+
+
+# Drop all unnecessary columns.
+for column in dataframe:
+    if column not in KEYS:
+        dataframe.drop(column, axis=1, inplace=True)
+
 
 # Turn data into actual numbers.
 for key in [
@@ -51,24 +68,43 @@ for key in [
     ]:
     dataframe[key] = pd.to_numeric(dataframe[key])
 
-mean = dataframe["GDP ($ per capita) dollars"].mean(skipna=True)
-median = dataframe["GDP ($ per capita) dollars"].median(skipna=True)
+
+dataframe.dropna(inplace=True)
+
+# z = np.abs(stats.zscore(dataframe["GDP ($ per capita) dollars"]))
+# print(z)
+
+# dataframe = dataframe[(np.abs(stats.zscore(dataframe["GDP ($ per capita) dollars"])) < 3)]
+
+dataframe = dataframe[(np.abs(stats.zscore(dataframe.select_dtypes(exclude='object'))) < 3).all(axis=1)]
+
+
+length_after = len(dataframe.index)
+
+# dataframe.drop("Suriname", axis=0, inplace=True)
+
+print(f"--------------------------------------")
+print(f"{length_before - length_after} rows with missing values or outliers were dropped.")
+
+
+mean = dataframe["GDP ($ per capita) dollars"].mean()
+median = dataframe["GDP ($ per capita) dollars"].median()
 mode = dataframe["GDP ($ per capita) dollars"].mode()
-std = dataframe["GDP ($ per capita) dollars"].std(skipna=True)
+std = dataframe["GDP ($ per capita) dollars"].std()
 
 print(f"--------------------------------------")
 print(f"GDP ($ per capita) dollars")
-print(f"mean: {mean}")
-print(f"median: {median}")
-print(f"mode: {mode[0]}")
-print(f"standard deviation: {std}")
+print(f"mean: {mean:.2f}")
+print(f"median: {median:.2f}")
+print(f"mode: {mode[0]:.2f}")
+print(f"standard deviation: {std:.2f}")
 
 # Save and show histogram.
-nr_of_bins = 80
 fig, ax = plt.subplots()
-dataframe["GDP ($ per capita) dollars"].plot.hist(bins=nr_of_bins,
+dataframe["GDP ($ per capita) dollars"].plot.hist(bins=NR_OF_BINS,
     title="GDP ($ per capita) dollars")
 ax.set_xlabel("dollars")
+plt.grid(b=True, axis="y")
 plt.savefig("GDP.png")
 plt.show()
 
@@ -80,21 +116,19 @@ maximum = dataframe["Infant mortality (per 1000 births)"].max()
 
 print(f"--------------------------------------")
 print(f"Infant mortality (per 1000 births)")
-print(f"minimum: {minimum}")
-print(f"first quartile: {q1}")
-print(f"median: {median}")
-print(f"third quartile: {q3}")
-print(f"maximum: {maximum}")
+print(f"minimum: {minimum:.2f}")
+print(f"first quartile: {q1:.2f}")
+print(f"median: {median:.2f}")
+print(f"third quartile: {q3:.2f}")
+print(f"maximum: {maximum:.2f}")
 print(f"--------------------------------------")
 
 dataframe["Infant mortality (per 1000 births)"].plot.box()
+plt.grid(b=True, axis="y")
 plt.savefig("Infant_mortality.png")
 plt.show()
 
-# Drop all unnecessary columns.
-for column in dataframe:
-    if column not in keys:
-        dataframe.drop(column, axis=1, inplace=True)
+
 
 json_data = dataframe.to_json(orient="index")
 
