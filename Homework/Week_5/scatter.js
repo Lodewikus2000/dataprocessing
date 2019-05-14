4415ec55c1b8970ea3123aafd020d9440cd82b06
Leo Schreuders <leoschreuders@hotmail.com>
2019-05-14 22:49:11 +0200

    beautiful maar niet af

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,9 +1,11 @@
 var w = 900;
 var h = 600;
 
+const labels = ["GDP", "% children (0-17) in violent areas", "Births per 1000 women aged 15-19",  "wealth share of top 10%",   ]
+
+// Make sure these are reflected in line 33
 
 
-var svg;
 
 
 window.onload = function() {
@@ -11,40 +13,25 @@ window.onload = function() {
 
   var teensInViolentArea = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA.CWB11/all?startTime=2010&endTime=2017"
   var teenPregnancies = "https://stats.oecd.org/SDMX-JSON/data/CWB/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA.CWB46/all?startTime=2009&endTime=2016"
-  var top1Share = "https://stats.oecd.org/SDMX-JSON/data/WEALTH/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA.ST1.TP/all?startTime=2009&endTime=2016"
+  var top1Share = "https://stats.oecd.org/SDMX-JSON/data/WEALTH/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA.ST10.TP/all?startTime=2009&endTime=2016"
   var GDP = "https://stats.oecd.org/SDMX-JSON/data/SNA_TABLE1/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA.B1_GE.HCPC/all?startTime=2012&endTime=2018&dimensionAtObservation=allDimensions"
+  var socialSpending = "https://stats.oecd.org/SDMX-JSON/data/SOCX_AGG/AUS+AUT+BEL+CAN+CHL+DNK+EST+FIN+FRA+DEU+GRC+HUN+IRL+ITA+JPN+KOR+LVA+LUX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+GBR+USA./all?startTime=2009&endTime=2016"
 
   var requests = [d3.json(teensInViolentArea), d3.json(teenPregnancies), d3.json(top1Share), d3.json(GDP)];
 
   Promise.all(requests).then(function(response) {
-      var teensInVioBetter = transformResponse(response[0]);
-      console.log(teensInVioBetter);
 
+      var teensInViolenceBetter = transformResponse(response[0]);
       var teenPregBetter = transformResponse(response[1]);
-      console.log(teenPregBetter);
-
       var top1Better = transformResponse(response[2]);
-      console.log(top1Better);
-
       var GDPBetter = transformResponse(response[3]);
-      console.log(GDPBetter);
 
+      // the order is [x, y, colored, radius]
+      var results = mergeData([GDPBetter, teensInViolenceBetter, teenPregBetter, top1Better]);
 
-      var results = mergeData([teensInVioBetter, teenPregBetter, top1Better, GDPBetter]);
-      console.log("results[0]")
-      console.log(results[0]);
-      console.log("results[1]");
-      console.log(results[1]);
-      console.log("results[2]");
-      console.log(results[2]);
-
-
-
-
-      scatter(results[0], results[1], results[2]);
+      scatter(results[0], results[1], results[2], labels);
       console.log(d3.select("#yearSelect").property("value"));
       scatter.update(d3.select("#yearSelect").property("value"));
-
 
       var select = d3.select("#yearSelect")
       .style("border-radius", "5px")
@@ -66,7 +53,9 @@ window.onload = function() {
 
 
 
-function scatter(dataset, years, countries) {
+function scatter(dataset, years, countries, labels) {
+
+
 
   var margin = {
       left: 50,
@@ -111,18 +100,76 @@ function scatter(dataset, years, countries) {
   svg.append("g")
       .attr("class", "y-axis");
 
-
+console.log(countries);
+console.log(countries.length);
+  var colScale = d3.scaleOrdinal()
+    .domain(countries)
+    .range(["#c75d27",
+"#616ddb",
+"#59c142",
+"#a654c9",
+"#89ba3d",
+"#d63d8a",
+"#55c974",
+"#d380d2",
+"#41922e",
+"#8457a1",
+"#b2b72d",
+"#6b86c9",
+"#d89c33",
+"#4ab3d2",
+"#d64243",
+"#56c4a5",
+"#a14a77",
+"#369657",
+"#e3829e",
+"#6d8c2e",
+"#ad4f50",
+"#83b26f",
+"#df936b",
+"#367e5c",
+"#92692d",
+"#5c7237",
+"#b5ae5e",
+"#6a701d"]);
   // scale for the color
   var c = d3.scaleLinear()
       .range(["deepskyblue", "gold"]);
 
+  var cFunction = function(d) {
+    console.log(d.Country);
+    return colScale[d.Country];
+  }
 
-  var minRadius = 8;
-  var maxRadius = 40;
+
   // scale for the radius
+  var minRadius = 4;
+  var maxRadius = 36;
   var r = d3.scaleLinear()
       .range([minRadius, maxRadius]);
 
+
+
+  svg.selectAll(".x-axis")
+      .append("text")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .style("fill", "black")
+      .text(labels[0]);
+
+  svg.selectAll(".y-axis")
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .style("fill", "black")
+      .text(labels[1])
+
+
+  var line = svg.append("line")
+        .attr("class", "regression");
 
 
   var tooltip = d3.select("body").append("div").attr("class", "tooltip");
@@ -146,6 +193,10 @@ update(d3.select("#yearSelect").property("value"))
     // even tijdelijk
 
     var yearData = dataset.filter(d => d.Time == year);
+
+
+    yearData = yearData.filter(d => (d.Datapoint[0]) && (d.Datapoint[0] === d.Datapoint[0])
+                                  && (d.Datapoint[1]) && (d.Datapoint[1] === d.Datapoint[1]))
     console.log("deze gaan we tekenen:");
     console.log(yearData);
 
@@ -162,10 +213,10 @@ update(d3.select("#yearSelect").property("value"))
 
 
     if (!maxX) {
-        maxX = 0;
+        maxX = 1;
     }
     if (!maxY) {
-        maxY = 0;
+        maxY = 1;
     }
     if (!maxC) {
         maxC = 1;
@@ -220,12 +271,7 @@ update(d3.select("#yearSelect").property("value"))
       return margin.top + y(d.Datapoint[1]);
     }
 
-    var cFunction = function(d) {
-      if (d.Datapoint[2] == null) {
-          return "grey";
-      };
-      return c(d.Datapoint[2]);
-    }
+
 
     var rFunction = function(d) {
       if (d.Datapoint[3] == null) {
@@ -237,12 +283,7 @@ update(d3.select("#yearSelect").property("value"))
 
     var circle = svg.selectAll("circle").data(yearData);
 
-
-    console.log(circle);
     circle.exit().remove();
-
-
-
 
     circle.enter().append("circle")
         .attr("class", "circle")
@@ -255,7 +296,7 @@ update(d3.select("#yearSelect").property("value"))
           return yFunction(d)
         })
         .attr("fill", function(d) {
-          return cFunction(d)
+          return colScale(d.Country)
         })
         .attr("r", function(d) {
           return rFunction(d)
@@ -263,38 +304,114 @@ update(d3.select("#yearSelect").property("value"))
 
 
 
+      var regression = getRegression();
+      console.log("dit komt eruit");
+      console.log(y(regression(minX)));
 
-        mouseOver(yearData);
+      console.log("eens kijken");
+      console.log(y(regression(0)))
+      console.log(y(regression(maxX)))
 
 
 
-        function mouseOver(yearData) {
+      line.merge(line).transition(t)
+          .attr("stroke-width", 2)
+          .attr("stroke", "black")
+
+          .attr("x1", margin.left + x(0))
+          .attr("y1", margin.top + y(regression(0)))
+          .attr("x2", margin.left + x(maxX))
+          .attr("y2", margin.top + y(regression(maxX)));
 
 
-          // Show the tooltip when the user mouses over the bars.
-          var circle = svg.selectAll(".circle").data(yearData)
-          var oldR;
-          circle.on("mousemove", function(d) {
-                  tooltip.style("left", (d3.event.pageX - 50) + "px")
-                      .style("top", (d3.event.pageY - 200) + "px")
-                      .style("display", "inline-block")
-                      .html(d.Country + "<br>" + "Teens in violent areas:<br>" + (d.Datapoint[0]) + "<br>Teen pregnancy rate:<br>" + (d.Datapoint[1]) + "<br>share of 1 percent:<br>" + (d.Datapoint[2]) + "<br>GDP:<br>" + (d.Datapoint[3]) );
-                  d3.select(this).attr("fill", "purple").attr("r", function(d) {
-                    return rFunction(d) * 2;
-                  });
-              })
-              .on("mouseout", function(d) {
-                  tooltip.style("display", "none");
-                  d3.select(this)
-                  .transition(t)
-                  .attr("fill", function(d) {
-                    return cFunction(d)
-                  })
-                  .attr("r", function(d) {
-                    return rFunction(d)
-                  });
-              });
+      mouseOver();
+
+
+
+      function getRegression() {
+        let regressionData = yearData.filter(d => (d.Datapoint[0]) && (d.Datapoint[0] === d.Datapoint[0]) && (d.Datapoint[1]) && (d.Datapoint[1] === d.Datapoint[1]));
+        console.log("selected data:")
+        console.log(regressionData);
+        // based on https://bl.ocks.org/ctufts/298bfe4b11989960eeeecc9394e9f118
+        let totalX = 0;
+        let totalY = 0;
+        let n = regressionData.length;
+        let xMean = 0;
+        let yMean = 0;
+        let term1 = 0;
+        let term2 = 0;
+
+        for (i = 0; i < n; i++) {
+            totalX += regressionData[i].Datapoint[0];
+            totalY += regressionData[i].Datapoint[1];
         };
+        xMean = totalX / n;
+        yMean = totalY / n;
+        console.log("xmean: " + xMean );
+        console.log("ymean: " + yMean);
+
+        // calculate coefficients
+        var xr = 0;
+        var yr = 0;
+        for (i = 0; i < n; i++) {
+            // console.log(regressionData[i].Datapoint[0])
+            // console.log(regressionData[i].Datapoint[1])
+            // console.log("jaaaaaaaaaaaaaaaaaaaaa");
+            xr = regressionData[i].Datapoint[0] - xMean;
+            yr = regressionData[i].Datapoint[1] - yMean;
+            term1 = term1 + xr * yr;
+            term2 = term2 + xr * xr;
+        };
+        console.log(term1);
+        console.log(term2);
+
+
+        var slope = term1 / term2;
+        var b = yMean - (slope * xMean);
+
+        if ( b !== b || slope !== slope) {
+          b = 0;
+          slope = 0;
+        }
+        console.log("b = " + b +  " slope = " + slope);
+        return function (d) {
+          return b + (d * slope)
+        }
+
+
+
+
+      };
+
+
+
+      function mouseOver() {
+
+
+        // Show the tooltip when the user mouses over the bars.
+        var circle = svg.selectAll(".circle").data(yearData)
+        var oldR;
+        circle.on("mousemove", function(d) {
+                tooltip.style("left", (d3.event.pageX - 50) + "px")
+                    .style("top", (d3.event.pageY - 200) + "px")
+                    .style("display", "inline-block")
+                    .html(d.Country + "<br>" + labels[0] + ":<br>" + (Math.round(d.Datapoint[0]*100)/100) + "<br>" + labels[1] +":<br>" + (Math.round(d.Datapoint[1]*100)/100) + "<br>" + labels[2] + ":<br>" + (Math.round(d.Datapoint[2]*100)/100) + "<br>" + labels[3] + ":<br>" + (Math.round(d.Datapoint[3]*100)/100) );
+                d3.select(this).attr("r", function(d) {
+                  return rFunction(d) * 1.4;
+                });
+            })
+            .on("mouseout", function(d) {
+                tooltip.style("display", "none");
+                d3.select(this)
+                .transition(t)
+                .attr("fill", function(d) {
+                  return colScale(d.Country)
+                })
+                .attr("r", function(d) {
+                  return rFunction(d)
+                });
+            });
+      };
 
 
   }
