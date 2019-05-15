ab1dbf9dcf4638000e1b809e539949b28b9284e9
Leo Schreuders <leoschreuders@hotmail.com>
2019-05-15 23:11:59 +0200

    alles werkt

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,9 +1,8 @@
 var w = 900;
 var h = 600;
 
+// The order of line 5 and line 29  (var results) have to match.
 const labels = ["GDP ($)", "births per 1000 women aged 15-19", "annual hours worked", "children (aged 0-17) in violent areas (%)"];
-
-
 
 
 
@@ -25,7 +24,8 @@ window.onload = function() {
       var GDPBetter = transformResponse(response[2]);
       var hoursWorkedBetter = transformResponse(response[3]);
 
-      // index 0 will be x-axis, index 4 will be radius
+      // The order of line 5 and line 29 have to match.
+      // Index 0 will be x-axis, 3 will be color.
       var results = mergeData([GDPBetter, teenPregBetter, hoursWorkedBetter, teensInViolenceBetter]);
 
 
@@ -54,29 +54,19 @@ window.onload = function() {
 
 
 
-
-
-
-
-
 function scatter(dataset, years, countries, labels) {
 
-
-
-
-
   var margin = {
-      left: 80,
+      left: 40,
       right: 30,
       top: 30,
-      bottom: 40
+      bottom: 100
   };
 
   var width = w - margin.left - margin.right;
   var height = h - margin.top - margin.bottom;
 
-  svg = d3.select("svg").attr("width", w).attr("height", h);
-
+  svg = d3.select("#main").attr("width", w).attr("height", h);
 
 
   var yearOptions = d3.select("#yearSelect").selectAll("option")
@@ -114,17 +104,9 @@ function scatter(dataset, years, countries, labels) {
       .attr("class", "y-axis");
 
 
-  // scale for the color
+  // Scale for the colors on the dots.
   var c = d3.scaleLinear()
-    .range(["#fee0d2", "#de2d26"]);
-
-
-
-  // scale for the radius
-  var minRadius = 4;
-  var maxRadius = 36;
-  var r = d3.scaleLinear()
-      .range([minRadius, maxRadius]);
+    .range(['#ffeda0','#f03b20']);
 
 
 
@@ -146,34 +128,71 @@ function scatter(dataset, years, countries, labels) {
       .style("text-anchor", "end")
       .style("fill", "black")
 
+
   var line = svg.append("line")
         .attr("class", "regression");
 
 
   var tooltip = d3.select(".tooltip");
 
-  // const g = svg.append('g')
-  //     .attr('transform', `translate(${margin.left},${margin.top})`);
-  // const colorLegendG = g.append('g')
-  //           .attr('transform', `translate(${60}, 150)`);
-  // colorLegendG.append('text')
-  //       .attr('class', 'legend-label')
-  //       .attr('x', -30)
-  //       .attr('y', -40)
-  //       .text("fuck");
-  // colorLegendG.call(c)
-  // .selectAll('.cell text')
-  //   .attr('dy', '0.1em');
+  // Scale needed for the color legend.
+  var dataToLegendScale = d3.scaleLinear()
+    .range([0, width]);
+
+
+  // set the color axis
+  var legendScale = d3.scaleLinear()
+      .range(['#ffeda0','#f03b20'])
+      .domain([0, width]);
+
+  // set a function for the color axis:
+  var colorAxis = g => g
+      .attr("transform", "translate(" + margin.left + "," + (h - margin.bottom / 4) + ")")
+      .call(d3.axisBottom(dataToLegendScale))
+
+  // Add an element to the svg for the x-axis.
+  svg.append("g")
+      .attr("class", "colorAxis")
+
+
+
+  svg.selectAll(".colorAxis")
+      .append("text")
+      .attr("x", width)
+      .attr("y", -16)
+      .style("text-anchor", "end")
+      .style("fill", "black")
+      .text([labels[3]])
+
+
+  var pallete = svg.append('g')
+    .attr('id', 'pallete');
+
+
+  var legendData = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0].map(x => x * width);
+  var colorHeight = 8
+  var swatch = pallete.selectAll('rect').data(legendData);
+  swatch.enter().append('rect')
+    .attr('fill', function(d) {
+      return legendScale(d);
+    })
+    .attr('x', function(d, i) {
+      return margin.left + i * (width / legendData.length);
+    })
+    .attr('y', h - margin.bottom / 4 - colorHeight)
+    .attr('width', width / legendData.length)
+    .attr('height', colorHeight);
+
+
 
 
   update(yearOptions.property("value"), yOptions.property("value") )
 
-  function update(year, yVar) {
 
+  function update(year, yVar) {
 
     var xIndex = 0;
     var yIndex = labels.indexOf(yVar);
-    var radiusIndex = 3;
     var colorIndex = 3;
 
 
@@ -182,21 +201,18 @@ function scatter(dataset, years, countries, labels) {
     var yearData = dataset.filter(d => d.Time == year);
 
 
-    yearData = yearData.filter(d => ((d.Datapoint[xIndex]) && d.Datapoint[xIndex] === d.Datapoint[xIndex]))
-                              //     && ( (d.Datapoint[yIndex]) && d.Datapoint[yIndex] === d.Datapoint[yIndex]))
+    yearData = yearData.filter(d => ((d.Datapoint[xIndex]) && d.Datapoint[xIndex] === d.Datapoint[xIndex])
+                                 && ( (d.Datapoint[yIndex]) && d.Datapoint[yIndex] === d.Datapoint[yIndex]))
 
 
     var maxX = d3.max(yearData, d => d.Datapoint[xIndex]);
     var maxY = d3.max(yearData, d => d.Datapoint[yIndex]);
-    var maxR = d3.max(yearData, d => d.Datapoint[radiusIndex]);
     var maxC = d3.max(yearData, d => d.Datapoint[colorIndex]);
 
 
     var minX = d3.min(yearData, d => d.Datapoint[xIndex]);
     var minY = d3.min(yearData, d => d.Datapoint[yIndex]);
-    var minR = d3.min(yearData, d => d.Datapoint[radiusIndex]);
     var minC = d3.min(yearData, d => d.Datapoint[colorIndex]);
-
 
 
     if (!maxX) {
@@ -205,11 +221,8 @@ function scatter(dataset, years, countries, labels) {
     if (!maxY) {
         maxY = 1;
     }
-    if (!maxR) {
-        maxR = 1;
-    }
     if (!maxC) {
-        maxC = 1;
+        maxC = 0;
     }
 
     if (!minX) {
@@ -218,27 +231,24 @@ function scatter(dataset, years, countries, labels) {
     if (!minY) {
         minY = 0;
     }
-    if (!minR) {
-        minR = 0;
-    }
     if (!minC) {
         minC = 0;
     }
-
 
 
     // The scalefactor makes the plot slightly bigger than the max values.
     var scaleFactor = 1.05
     x.domain([0, maxX * scaleFactor]);
     y.domain([0, maxY * scaleFactor]);
-    r.domain([minR, maxR]);
     c.domain([minC, maxC])
 
-
+    dataToLegendScale.domain([minC, maxC]);
 
     svg.selectAll(".y-axis").transition(t).call(yAxis).select(".text")
         .text(labels[yIndex]);
     svg.selectAll(".x-axis").transition(t).call(xAxis);
+    svg.selectAll(".colorAxis").transition(t).call(colorAxis);
+
 
 
 
@@ -263,13 +273,6 @@ function scatter(dataset, years, countries, labels) {
       return c(d.Datapoint[colorIndex]);
     }
 
-    var rFunction = function(d) {
-      // if (d.Datapoint[radiusIndex] == null) {
-      //     return r(minR);
-      // };
-      // return r(d.Datapoint[radiusIndex]);
-      return 8;
-    }
 
 
     var circle = svg.selectAll("circle").data(yearData);
@@ -291,9 +294,7 @@ function scatter(dataset, years, countries, labels) {
         .attr("fill", function(d) {
           return cFunction(d)
         })
-        .attr("r", function(d) {
-          return rFunction(d)
-        });
+        .attr("r", 8);
 
 
       var regression = getRegression();
@@ -301,8 +302,7 @@ function scatter(dataset, years, countries, labels) {
 
       line.merge(line).transition(t)
           .attr("stroke-width", 2)
-          .attr("stroke", "black")
-
+          .attr("stroke", "#404040")
           .attr("x1", margin.left + x(0))
           .attr("y1", margin.top + y(regression(0)))
           .attr("x2", margin.left + x(maxX))
@@ -313,18 +313,14 @@ function scatter(dataset, years, countries, labels) {
 
 
 
-
-
-
-
-
       function getRegression() {
         // Least squares method.
+        // Based on https://bl.ocks.org/ctufts/298bfe4b11989960eeeecc9394e9f118
 
         // Don't use empty datapoints.
         let regressionData = yearData.filter(d => (d.Datapoint[xIndex]) && (d.Datapoint[xIndex] === d.Datapoint[xIndex]) && (d.Datapoint[yIndex]) && (d.Datapoint[yIndex] === d.Datapoint[yIndex]));
 
-        // Based on https://bl.ocks.org/ctufts/298bfe4b11989960eeeecc9394e9f118
+
         let totalX = 0;
         let totalY = 0;
         let n = regressionData.length;
@@ -363,7 +359,6 @@ function scatter(dataset, years, countries, labels) {
           return b + (d * slope)
         }
 
-
       };
 
 
@@ -373,21 +368,17 @@ function scatter(dataset, years, countries, labels) {
         var circle = svg.selectAll(".circle");
         var oldR;
         circle.on("mousemove", function(d) {
-                tooltip.style("left", (d3.event.pageX + 40) + "px")
-                    .style("top", (d3.event.pageY - 140) + "px")
+                tooltip.style("left", (d3.event.pageX + 20) + "px")
+                    .style("top", (d3.event.pageY - 120) + "px")
                     .style("display", "inline-block")
-                    .html(d.Country + "<br>" + labels[xIndex] + ":<br>" + (Math.round(d.Datapoint[xIndex]*100)/100) + "<br>" + labels[yIndex] +":<br>" + d.Datapoint[yIndex] + "<br>" + labels[colorIndex] + ":<br>" + d.Datapoint[colorIndex]);
-                d3.select(this).attr("r", function(d) {
-                  return rFunction(d) * 1.4;
-                });
+                    .html("<b>" + d.Country + "</b>" + "<br>" + labels[xIndex] + ": <b>" + (Math.round(d.Datapoint[xIndex]*100)/100) + "</b> <br>" + labels[yIndex] +": <b>" + d.Datapoint[yIndex] + "</b> <br>" + labels[colorIndex] + ": <b>" + d.Datapoint[colorIndex]) + "</b>";
+                d3.select(this).attr("r", 16);
             })
             .on("mouseout", function(d) {
                 tooltip.style("display", "none");
                 d3.select(this)
                 .transition(t)
-                .attr("r", function(d) {
-                  return rFunction(d)
-                });
+                .attr("r", 8);
             });
       };
 
@@ -396,18 +387,13 @@ function scatter(dataset, years, countries, labels) {
 
   scatter.update = update;
 
-
 };
-
-
-
-
 
 
 
 function mergeData(dataArray) {
   // Returns [results, years, countries]
-  // Here we just merge all data together in an object with country as key, an array with data as value.
+  // Here we merge all data together in an object with country as key, an array with data as value.
 
   var countries = new Set();
   for (var i = 0; i < dataArray.length; i++) {
@@ -519,25 +505,22 @@ function transformResponse(data) {
   });
 
 
-  // get the time periods
+  // Get the time periods.
   let observation;
   if (seriesBool){
     observation = data.structure.dimensions.observation.filter(d => d.id == "TIME_PERIOD")[0];
-    // also add the time periods as a variable
+    // Also add the time periods as a variable.
     varArray.push(observation);
   } else {
     observation = data.structure.dimensions.observation.filter(d => d.id == "TIME_PERIOD")[0];
-    // varArray.push(observation);
   };
 
   // This is an object with all combinations of the 0:0:0 format.
   // They don't necessarily all have a value, but they are all possible datapoints.
   let strings = Object.keys(dataHere);
 
-
   // The output object has country as key and an array as value.
   let dataObject = {};
-
 
   strings.forEach(function(string) {
 
@@ -564,24 +547,20 @@ function transformResponse(data) {
           tempString = string.split(":");
         };
 
-        // I think here we set names and explanations of variables?
-        tempString.forEach(function(s, indexi) {
-          // Name of the variable will be the key, value will be the value.
-          tempObj[varArray[indexi].name] = varArray[indexi].values[s].name;
 
+        tempString.forEach(function(s, indexi) {
+          tempObj[varArray[indexi].name] = varArray[indexi].values[s].name;
         });
 
         // Every datapoint also needs a timestamp and actual datapoint.
         if (seriesBool) {
           tempObj["Time"] = observ.name;
         } else {
-
           if (tempObj["Year"]) {
             tempObj["Time"] = tempObj["Year"];
           } else {
             tempObj["Time"] = tempObj["Time"];
           }
-
         };
 
 
@@ -596,7 +575,7 @@ function transformResponse(data) {
         }
 
 
-        // Add the temporary object to thte total object
+        // Add the temporary object to the total object.
         if (dataObject[tempObj["Country"]] == undefined) {
           dataObject[tempObj["Country"]] = [tempObj];
         } else if (seriesBool) {
