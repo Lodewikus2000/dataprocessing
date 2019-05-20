3efa7db0739f774d28ef45db50bc89dc12221900
Lodewikus2000 <32392003+Lodewikus2000@users.noreply.github.com>
2019-05-20 22:46:03 +0200

    kleurtjes

--- Homework/Week_6/linkedviews.js
+++ Homework/Week_6/linkedviews.js
@@ -1,7 +1,7 @@
 var w = 800;
 var h = 600;
 
-
+const YEAR = 2014;
 
 window.onload = function() {
 
@@ -14,7 +14,29 @@ window.onload = function() {
     console.log(response[0]);
     console.log(response[1]);
 
-    var map = new Datamap({element: document.getElementById("mapContainer")});
+    var years = new Set();
+
+    for (i = 0; i < response[0].length; i++) {
+      years.add(response[0][i].TIME);
+    }
+    years = Array.from(years);
+    years.sort();
+
+    var yearOptions = d3v5.select("#yearSelect").selectAll("option")
+        .data(years.reverse())
+        .enter().append("option")
+        .text(d => d);
+
+
+
+
+
+    drawMap(response[0], yearOptions.property("value"));
+    console.log(response[0]);
+
+    var yearOptions = d3v5.select("#yearSelect").on("change", function() {
+        drawMap.update(this.value);
+    });
 
     // einde promise -> then function
   });
@@ -25,6 +47,78 @@ window.onload = function() {
 
 
 
-function drawMap(dataset, years, countries, labels) {
+function drawMap(dataset, year) {
+  drawMap.update = update;
+  // help from http://jsbin.com/kuvojohapi/1/edit?html,output
+
+  var dataHere = dataset.filter( d => (d.TIME == year) );
+
+  var giniMax = d3v5.max(dataHere, d => d.Value);
+  var giniMin = d3v5.min(dataHere, d => d.Value);
+
+  var colorMap = {};
+
+
+  var colorScale = d3v5.scaleLinear()
+    .range(['#ffff4d','#ff4d4d'])
+    .domain([0,1]);
+
+
+  dataHere.forEach(function(item) {
+      var iso = item.LOCATION;
+      var value = item.Value;
+      colorMap[iso] = { numberOfThings: value, fillColor: colorScale(value) }
+  });
+
+
+
+  var map = new Datamap({
+    element: document.getElementById("mapContainer"),
+    done: function(datamap) {
+                datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography, data) {
+                    console.log("data:");
+                    console.log(data);
+                    console.log("geography:");
+                    console.log(geography);
+                    console.log("de overeenkomende data:");
+                    console.log(dataHere.filter( d => d.LOCATION == geography.id ));
+                });
+            },
+  data: colorMap,
+  highlightFillColor: function(geo) {
+                return geo['fillColor'];
+            },
+  // default color
+  fills: { defaultFill: '#989898' },
+
+
+
+  });
+
+
+  function update(year) {
+    console.log("in the update function!");
+    dataHere = dataset.filter( d => (d.TIME == year) );
+    dataHere = dataset.filter( d => (d.TIME == year) );
+
+    giniMax = d3v5.max(dataHere, d => d.Value);
+    giniMin = d3v5.min(dataHere, d => d.Value);
+
+    colorMap = {};
+
+
+    colorScale.domain([0,1]);
+
+
+    dataHere.forEach(function(item) {
+        var iso = item.LOCATION;
+        var value = item.Value;
+        colorMap[iso] = { numberOfThings: value, fillColor: colorScale(value) }
+    });
+
+    map.updateChoropleth(colorMap);
+
+  };
+
 
 }
