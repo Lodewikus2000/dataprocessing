910194dbc17eeec5bb9a06dd780983f8000e219b
Lodewikus2000 <leoschreuders@hotmail.com>
2019-05-29 14:53:41 +0200

    code mag mooier

--- Homework/Week_6/linkedviews.js
+++ Homework/Week_6/linkedviews.js
@@ -11,59 +11,53 @@ var margin = {
     bottom: 40
 };
 
-const YEAR = 2014;
+const DEFAULTYEAR = 2015;
+const DEFAULTCOUNTRY = "CHN";
 
 window.onload = function() {
 
+    var requests = [d3v5.json("data/life_expectancy_after.json")]
 
-  var requests = [d3v5.json("data/life_expectancy_after.json")]
-
-  Promise.all(requests).then(function(response) {
-
-
-    var years = new Set();
-
-    for (i = 0; i < response[0].length; i++) {
-      years.add(response[0][i].Year);
-    }
-    years = Array.from(years);
-    years.sort();
-
-    var yearOptions = d3v5.select("#yearSelect").selectAll("option")
-        .data(years.reverse())
-        .enter().append("option")
-        .text(d => d);
+    Promise.all(requests).then(function(response) {
 
 
+        var years = new Set();
 
-    life_expectancy_total = response[0].filter(d => d.Variable == "Total population at birth");
+        for (i = 0; i < response[0].length; i++) {
+          years.add(response[0][i].Year);
+        }
+        years = Array.from(years);
+        years.sort();
 
-    drawMap(life_expectancy_total, yearOptions.property("value"));
+        var yearOptions = d3v5.select("#yearSelect").selectAll("option")
+            .data(years.reverse())
+            .enter().append("option")
+            .property("selected", d => d === DEFAULTYEAR)
+            .text(d => d);
 
 
-    var yearOptions = d3v5.select("#yearSelect").on("change", function() {
-        drawMap.update(this.value, 750);
+        life_expectancy_total = response[0].filter(d => d.Variable == "Total population at birth");
+
+        drawMap(life_expectancy_total, DEFAULTYEAR);
+
+
+        var yearOptions = d3v5.select("#yearSelect").on("change", function() {
+            drawMap.update(this.value, 750);
+        });
+
+
+        dataCountry = response[0].filter(d => d.COU == DEFAULTCOUNTRY);
+        drawLineGraph(response[0]);
+        drawLineGraph.update(DEFAULTCOUNTRY);
+
     });
-
-
-    dataCountry = response[0].filter(d => d.COU == "USA");
-    drawLineGraph(response[0]);
-    drawLineGraph.update("USA");
-
-
-
-    // einde promise -> then function
-  });
-
-
-// einde onload
 };
 
 
 function drawLineGraph(dataset) {
     drawLineGraph.update = update;
 
-    // help from https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
+    // Used help from https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0/
 
     var dataHere = dataset;
 
@@ -72,30 +66,14 @@ function drawLineGraph(dataset) {
 
     yearMax = d3v5.max(dataHere, d => d.Year);
     yearMin = d3v5.min(dataHere, d => d.Year);
-    console.log(yearMin);
-    console.log(yearMax);
-
-
 
 
 
     var svg = d3v5.select("#lineSVG").attr("width", w).attr("height", h);
 
 
-
-
-
-
     const tooltip = d3v5.select('#tooltip');
     const tooltipLine = svg.append('line');
-
-
-
-
-
-
-
-
 
 
     // Scale for x.
@@ -105,10 +83,18 @@ function drawLineGraph(dataset) {
     // Function for x axis.
     var xAxis = g => g
         .attr("transform", "translate(" + margin.left + "," + (margin.top + height) + ")")
-        .call(d3v5.axisBottom(xScale));
+        .call(d3v5.axisBottom(xScale)
+            .tickFormat(d3v5.format("d"))
+        );
     // Element for x.
     svg.append("g")
-        .attr("class", "x-axis");
+        .attr("class", "x-axis")
+        .append("text")
+        .attr("x", width)
+        .attr("y", - 8)
+        .style("text-anchor", "end")
+        .style("fill", "black")
+        .text("year");
     // Set x axis.
     svg.selectAll(".x-axis").call(xAxis);
 
@@ -121,7 +107,16 @@ function drawLineGraph(dataset) {
         .call(d3v5.axisLeft(yScale));
     // Element for y.
     svg.append("g")
-        .attr("class", "y-axis");
+        .attr("class", "y-axis")
+        .append("text")
+
+        .attr("x", - height * .8 )
+        .attr("y", 16)
+        .style("text-anchor", "start")
+
+        .attr("transform", "rotate(-90)")
+        .style("fill", "black")
+        .text("life expectancy (years)");
 
 
     // Define the line for the graph.
@@ -131,11 +126,38 @@ function drawLineGraph(dataset) {
 
 
     var title = svg.append("text")
-    .attr("x", w / 2)
-    .attr("y",  16)
-    .attr("class", "lineTitle")
-    .style("text-anchor", "start")
-    .style("fill", "black");
+        .attr("x", w / 2)
+        .attr("y",  16)
+        .attr("class", "lineTitle")
+        .style("text-anchor", "middle")
+        .style("fill", "black");
+
+
+
+    // Draw the legend with help from https://stackoverflow.com/questions/38954316/adding-legends-to-d3-js-line-charts.
+    var legendBoxSize = 10;
+    var legendBoxDistance = 16;
+
+    var legend_keys = ["Females", "Total", "Males"]
+
+    var lineLegend = svg.selectAll(".lineLegend").data(legend_keys)
+        .enter().append("g")
+        .attr("class", "lineLegend")
+        .attr("transform", "translate(" + (margin.left + width * .8) + "," + (height * .8) + ")");
+
+    lineLegend.append("text").text(d => d)
+        .attr("class", "legendText")
+        .attr("x", legendBoxDistance)
+        .attr("y", function(d, i) {
+            return i * legendBoxDistance;
+        });
+
+    lineLegend.append("rect")
+        .attr("class", d => d + "Legend")
+        .attr("width", legendBoxSize).attr("height", legendBoxSize)
+        .attr("y", function(d, i) {
+            return i * legendBoxDistance - (legendBoxSize);
+        });
 
 
 
@@ -156,7 +178,7 @@ function drawLineGraph(dataset) {
         yScale.domain([0, ageMax * scaleFactor]);
 
 
-
+        // Lines.
 
         var lineTotal = svg.selectAll(".lineTotal").data([countryDataTotal]);
         lineTotal.exit().remove();
@@ -188,18 +210,15 @@ function drawLineGraph(dataset) {
 
         svg.selectAll(".y-axis").transition(t).call(yAxis);
 
-
-
+        // Title.
         if (countryDataTotal.length > 0) {
-            title.transition(t).text("life expectancy in " + countryDataTotal[0].Country );
+            title.transition(t).text("Life expectancy in " + countryDataTotal[0].Country );
         } else {
             title.transition(t).text();
         }
 
 
-
-
-
+        // Dots.
 
         var dotTotal = svg.selectAll(".dotTotal").data(countryDataTotal);
         dotTotal.exit().remove();
@@ -210,7 +229,7 @@ function drawLineGraph(dataset) {
             .transition(t)
             .attr("cx", function(d) { return xScale(d.Year) })
             .attr("cy", function(d) { return yScale(d.Value) })
-            .attr("r", 3);
+            .attr("r", 2);
 
         var dotFemales = svg.selectAll(".dotFemales").data(countryDataFemales);
         dotFemales.exit().remove();
@@ -221,7 +240,7 @@ function drawLineGraph(dataset) {
             .transition(t)
             .attr("cx", function(d) { return xScale(d.Year) })
             .attr("cy", function(d) { return yScale(d.Value) })
-            .attr("r", 3);
+            .attr("r", 2);
 
         var dotMales = svg.selectAll(".dotMales").data(countryDataMales);
         dotMales.exit().remove();
@@ -232,7 +251,7 @@ function drawLineGraph(dataset) {
             .transition(t)
             .attr("cx", function(d) { return xScale(d.Year) })
             .attr("cy", function(d) { return yScale(d.Value) })
-            .attr("r", 3);
+            .attr("r", 2);
 
 
 
@@ -295,13 +314,8 @@ function drawLineGraph(dataset) {
 
 
 
-
-
-
-        // einde update
     }
 
-    // einde line graph
 }
 
 
@@ -310,26 +324,26 @@ function drawLineGraph(dataset) {
 
 
 function drawMap(dataset, year) {
+    // The year determines what year the map shows initially.
+
     drawMap.update = update;
     // help from http://jsbin.com/kuvojohapi/1/edit?html,output
 
     d3v5.select("#mapContainer").style("width", w + "px").style("height", h +"px").style("position", "relative");
 
 
-
-
     var dataHere = dataset;
 
     var defaultFillColor = '#B8B8B8';
     var colorScale = d3v5.scaleLinear()
-    .range(['#f03b20','#ffeda0'])
+        .range(['#f03b20','#ffeda0'])
 
 
     var colorMap = {};
     dataHere.forEach(function(item) {
-      var iso = item.COU;
-      var value = item.Value;
-      colorMap[iso] = { numberOfThings: value, fillColor: defaultFillColor }
+        var iso = item.COU;
+        var value = item.Value;
+        colorMap[iso] = { numberOfThings: value, fillColor: defaultFillColor }
     });
 
 
@@ -337,13 +351,7 @@ function drawMap(dataset, year) {
     element: document.getElementById("mapContainer"),
     done: function(datamap) {
                 datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography, data) {
-                    // console.log("data:");
-                    // console.log(data);
-                    console.log("geography:");
-                    console.log(geography);
                     drawLineGraph.update(geography.id, 500);
-                    // console.log("de overeenkomende data:");
-                    // console.log(dataHere.filter( d => d.COU == geography.id ));
                 });
             },
     geographyConfig: {
@@ -354,6 +362,7 @@ function drawMap(dataset, year) {
                 return defaultFillColor;
             };
         },
+        borderColor: "909090",
         highlightBorderColor: "#303030",
         popupTemplate: function(geo, data) {
             if (!data || !data.numberOfThings ){
@@ -378,52 +387,48 @@ function drawMap(dataset, year) {
     });
 
 
-    // hier nog even de legenda
-    // Scale needed for the color legend.
+
     var dataToLegendScale = d3v5.scaleLinear()
-    .range([legendHeight, 0]);
+        .range([legendHeight, 0]);
 
 
-    // set the color axis
     var legendScale = d3v5.scaleLinear()
-      .range(['#f03b20','#ffeda0'])
-      .domain([0, legendHeight]);
+        .range(['#f03b20','#ffeda0'])
+        .domain([0, legendHeight]);
 
-    // set a function for the color axis:
+
     var legendAxis = g => g
-      .attr("transform", "translate(" + ( margin.left + legendWidth ) +"," + (h - margin.bottom - legendHeight) + ")")
-      .call(d3v5.axisRight(dataToLegendScale));
+        .attr("transform", "translate(" + ( margin.left + legendWidth ) +"," + (h - margin.bottom - legendHeight) + ")")
+        .call(d3v5.axisRight(dataToLegendScale));
 
-    // Add an element to the svg for the x-axis.
 
     svg = d3v5.select(".datamap");
 
+    // Reserve an element for the colorLegend's axis
     svg.append("g")
-      .attr("class", "colorAxis");
+        .attr("class", "colorAxis");
 
     svg.selectAll(".colorAxis")
-      .append("text")
-      .attr("x", - legendWidth)
-      .attr("y", - 16)
-      .style("text-anchor", "start")
-      .style("fill", "black")
-      .text("years");
+        .append("text")
+        .attr("x", - legendWidth)
+        .attr("y", - 8)
+        .style("text-anchor", "start")
+        .style("fill", "black")
+        .text("total life expectancy (years)");
 
-
+    // Title.
     svg.append("text")
-    .attr("x", w / 2)
-    .attr("y",  16)
-    .attr("class", "mapTitle")
-    .style("text-anchor", "start")
-    .style("fill", "black")
-    .text("life expectancy across the world");
-
-
+        .attr("x", w / 2)
+        .attr("y",  16)
+        .attr("class", "mapTitle")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("Life expectancy across the world");
 
 
 
     var pallete = svg.append('g')
-      .attr('id', 'pallete');
+        .attr('id', 'pallete');
 
 
     var legendData = []
@@ -433,17 +438,15 @@ function drawMap(dataset, year) {
 
     var colorBar = pallete.selectAll('rect').data(legendData);
     colorBar.enter().append('rect')
-      .attr('fill', function(d) {
-        return legendScale(d);
-      })
-      .attr('x', margin.left )
-      .attr('y', function(d, i) {
-          return h - margin.bottom - (i + 1) * (legendHeight / legendData.length);
-      })
-      .attr('width', legendWidth )
-      .attr('height', legendHeight / legendData.length);
-
-
+        .attr('fill', function(d) {
+            return legendScale(d);
+        })
+        .attr('x', margin.left )
+        .attr('y', function(d, i) {
+            return h - margin.bottom - (i + 1) * (legendHeight / legendData.length);
+        })
+        .attr('width', legendWidth )
+        .attr('height', legendHeight / legendData.length);
 
 
 
@@ -454,39 +457,27 @@ function drawMap(dataset, year) {
 
         let t = d3v5.transition().duration(speed);
 
-        console.log("in the update function!");
         let yearData = dataHere.filter( d => (d.Year == year) );
 
         var ageMax = d3v5.max(yearData, d => d.Value);
         var ageMin = d3v5.min(yearData, d => d.Value);
 
-        // For the colors of the countries/
+
         colorScale.domain([ageMin, ageMax]);
 
-        // Set the axis of the legend.
+        // Set and update the axis of the legend.
         dataToLegendScale.domain([ageMin, ageMax]);
         svg.selectAll(".colorAxis").transition(t).call(legendAxis);
 
 
-
-
-
-
-
-        //colorMap = {};
-        console.log("hier");
-        console.log(colorMap);
-
-
-
-        // Set all colors back to grey
+        // Set all country colors back to default.
         Object.values(colorMap).forEach(function(d) {
           d.numberOfThings = null;
           d.fillColor = defaultFillColor;
         });
 
 
-        // and update the countries that have data
+        // Update the colors of countries with data.
         yearData.forEach(function(item) {
             var iso = item.COU;
             var value = item.Value;
@@ -496,8 +487,6 @@ function drawMap(dataset, year) {
 
         map.updateChoropleth(colorMap);
 
-
-        // en hier nog even de legenda updaten
 
         };
 
